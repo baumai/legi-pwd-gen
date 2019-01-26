a3350c5a01a7ea7333e2019aafdd1a545968d669
unknown <baumann.maik@burgward.de>
2019-01-26 14:48:03 +0100

    GPL to AGPL

--- src/main/java/de/burgward/legi_pwd_gen/SystemOutput.java
+++ src/main/java/de/burgward/legi_pwd_gen/SystemOutput.java
@@ -55,17 +55,17 @@ public class SystemOutput {
 		System.out.println("Copyright (C) 2019 Maik Baumann");
 		System.out.println("");
 		System.out.println("LegiblyPasswordGenerator is free software: you can redistribute it and/or modify");
-		System.out.println("it under the terms of the GNU General Public License as published by");
+		System.out.println("it under the terms of the GNU Affero General Public License as published by");
 		System.out.println("the Free Software Foundation, either version 3 of the License, or");
 		System.out.println("(at your option) any later version.");
 		System.out.println("");
 		System.out.println("LegiblyPasswordGenerator is distributed in the hope that it will be useful,");
 		System.out.println("but WITHOUT ANY WARRANTY; without even the implied warranty of");
 		System.out.println("MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the");
-		System.out.println("GNU General Public License for more details.");
+		System.out.println("GNU Affero General Public License for more details.");
 		System.out.println("");
-		System.out.println("You should have received a copy of the GNU General Public License");
-		System.out.println("along with LegiblyPasswordGenerator.  If not, see <https://www.gnu.org/licenses/>.");
+		System.out.println("You should have received a copy of the GNU Affero General Public License");
+		System.out.println("along with LegiblyPasswordGenerator. If not, see <http://www.gnu.org/licenses/>.");
 		System.out.println("");
 		System.out.println("");
 	}
