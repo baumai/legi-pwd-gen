0819e99d899ffb86ace40b5daab1dcae5fcfdfdc
unknown <baumann.maik@burgward.de>
2019-01-26 13:36:59 +0100

    GPL to AGPL

--- src/main/java/de/burgward/legi_pwd_gen/LegiblyPasswordGenerator.java
+++ src/main/java/de/burgward/legi_pwd_gen/LegiblyPasswordGenerator.java
@@ -3,17 +3,17 @@
  * Copyright (C) 2019 Maik Baumann
  *
  * LegiblyPasswordGenerator is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
  * LegiblyPasswordGenerator is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License
- * along with LegiblyPasswordGenerator.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with LegiblyPasswordGenerator. If not, see <http://www.gnu.org/licenses/>.
 */
 package de.burgward.legi_pwd_gen;
 
